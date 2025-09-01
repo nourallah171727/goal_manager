953bf36b35a126e38270d2a2b4d9db3b0c459e3b
ScriptSamurai-afbh <benhmidenfiras08@gmail.com>
2025-09-01 14:11:28 +0200

    entity done

--- src/main/resources/db/migration/V5__create_task_table.sql
+++ src/main/resources/db/migration/V5__create_task_table.sql
@@ -3,7 +3,7 @@ CREATE TYPE task_status AS ENUM ('DONE', 'NOT_DONE');
 
 CREATE TABLE IF NOT EXISTS tasks (
     task_id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(20) NOT NULL,
     status task_status NOT NULL DEFAULT 'NOT_DONE',
     task_goal BIGINT NOT NULL REFERENCES goals(id)
 );
