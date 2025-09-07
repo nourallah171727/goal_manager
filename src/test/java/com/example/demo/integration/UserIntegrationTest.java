691cab4f3638fbb58715e46cae502bc2cbf50f00
Your Name <gargourinourallah@gmail.com>
2025-09-07 18:37:22 +0100

    basic crud repo testing

--- src/test/java/com/example/demo/integration/UserIntegrationTest.java
+++ src/test/java/com/example/demo/integration/UserIntegrationTest.java
@@ -27,4 +27,23 @@ class UserRepositoryTest {
         assertThat(found.get().getUsername()).isEqualTo("alice");
         assertThat(found.get().getEmail()).isEqualTo("alice@example.com");
     }
+    @Test
+    void testUpdateUser(){
+        User user=new User("firas ben hmiden","firasBenHmiden@gmail.com");
+        userRepository.save(user);
+        user.setEmail("anothermail@gmail.com");
+        Optional<User> found=userRepository.findById(user.getId());
+        assertThat(found).isPresent();
+        assertThat(found.get().getUsername()).isEqualTo("firas ben hmiden");
+        assertThat(found.get().getEmail()).isEqualTo("anothermail@gmail.com");
+    }
+    @Test
+    void testDeleteUser(){
+        User user =new User("nourallah","nourallah@gmail.com");
+        userRepository.save(user);
+        Optional<User>found =userRepository.findById(user.getId());
+        assertThat(found).isPresent();
+        userRepository.deleteById(user.getId());
+        assertThat(userRepository.findById(user.getId())).isEmpty();
+    }
 }
