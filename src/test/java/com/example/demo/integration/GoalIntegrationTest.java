138438ff3e062e7b4d2fb77239f5cd30ba23ed2d
Your Name <gargourinourallah@gmail.com>
2025-09-07 19:57:50 +0100

    testing save of goal

--- src/test/java/com/example/demo/integration/GoalIntegrationTest.java
+++ src/test/java/com/example/demo/integration/GoalIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -30,19 +32,25 @@ class GoalRepositoryTest {
         userRepository.save(user);
         goalRepository.save(goal);
         Optional<Goal>found=goalRepository.findById(goal.getId());
-        assertThat(found).isPresent();
-        assertThat(found.get().equals(goal));
-        assertThat(user.getGoals().contains(found.get()));
+        Assertions.assertTrue((found).isPresent(),"found is not present");
+        Assertions.assertEquals(found.get(),goal,"found and goal are not equal");
+        Assertions.assertFalse(user.getGoals().isEmpty(),"user's goals are empty");
+        Assertions.assertTrue(user.getGoals().contains(found.get()),"user does not have the saved goal");
     }
     @Test
-    void testUpdateUser(){
-        User user=new User("firas ben hmiden","firasBenHmiden@gmail.com");
+    void testUpdateGoal(){
+        User user=new User("ahmed","ahmed@gmail.com");
+        Goal goal=new Goal("name",user);
+        Assertions.assertEquals(goal.getGoalStand(), GoalStand.NOT_STARTED);
         userRepository.save(user);
-        user.setEmail("anothermail@gmail.com");
-        Optional<User> found=userRepository.findById(user.getId());
+        goalRepository.save(goal);
+        Optional<Goal>found=goalRepository.findById(goal.getId());
         assertThat(found).isPresent();
-        assertThat(found.get().getUsername()).isEqualTo("firas ben hmiden");
-        assertThat(found.get().getEmail()).isEqualTo("anothermail@gmail.com");
+        found.get().setName("pipoupa");
+        goalRepository.save(found.get());
+        Optional<Goal>newGoal=goalRepository.findById(found.get().getId());
+        Assertions.assertEquals(newGoal.get(),found.get());
+        Assertions.assertTrue(user.getGoals().contains(newGoal));
     }
     @Test
     void testDeleteUser(){
