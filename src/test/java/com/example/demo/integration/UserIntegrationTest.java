c572d081535b27e407ed08a4f6b7f279f1e8fd15
ScriptSamurai-afbh <benhmidenfiras08@gmail.com>
2025-09-09 18:59:57 +0200

    finished task integration test

--- src/test/java/com/example/demo/integration/UserIntegrationTest.java
+++ src/test/java/com/example/demo/integration/UserIntegrationTest.java
@@ -1,49 +1,144 @@
 package com.example.demo.integration;
 
+import com.example.demo.model.Goal;
+import com.example.demo.model.GoalStand;
+import com.example.demo.model.Task;
 import com.example.demo.model.User;
+import com.example.demo.repository.GoalRepository;
+import com.example.demo.repository.TaskRepository;
 import com.example.demo.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.sql.ast.tree.from.TableAliasResolver;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Iterator;
 import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class UserRepositoryTest {
-
+public class TaskIntegrationTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    void testSaveAndFindUser() {
-        User user = new User("alice", "alice@example.com");
-        userRepository.save(user);
+    @Autowired
+    private GoalRepository goalRepository;
 
-        Optional<User> found = userRepository.findById(user.getId());
+    @Autowired
+    private TaskRepository taskRepository;
 
-        Assertions.assertTrue(found.isPresent());
-        Assertions.assertEquals(found.get(),user);
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private User user;
+    private Goal goal;
+    private Task task;
+
+    @BeforeEach
+    void setup(){
+        user = new User("hacker12.5", "thisisanemail@gmail.de");
+        userRepository.save(user);
+        entityManager.flush();
+
+        goal = new Goal("get 1.0", user);
+        goalRepository.save(goal);
+        entityManager.flush();
+
+        task = new Task("learn the course", goal);
+        taskRepository.save(task);
+        entityManager.flush();
     }
+
     @Test
-    void testUpdateUser(){
-        User user=new User("firas ben hmiden","firasBenHmiden@gmail.com");
-        userRepository.save(user);
-        user.setEmail("anothermail@gmail.com");
-        userRepository.save(user);
-        Optional<User> found=userRepository.findById(user.getId());
-        Assertions.assertTrue(found.isPresent());
-       Assertions.assertEquals(found.get(),user);
+    void saveTask(){
+        //correctness of the task object
+        entityManager.clear();
+        Optional<Task> taskDB = taskRepository.findById(task.getId());
+        assertTrue(taskDB.isPresent());
+        assertEquals(task.getName(), taskDB.get().getName());
+        assertNotNull(taskDB.get().getGoal());
+        assertEquals(goal.getName(), taskDB.get().getGoal().getName());
+        assertNotNull(taskDB.get().getGoal().getUser());
+        assertEquals(user.getUsername(), taskDB.get().getGoal().getUser().getUsername());
+        assertEquals(user.getEmail(), taskDB.get().getGoal().getUser().getEmail());
+
+        //correctness of the goal object
+        entityManager.clear();
+        Optional<Goal> goalDB = goalRepository.findById(goal.getId());
+        assertTrue(goalDB.isPresent());
+        Set<Task> taskGoalDB = goalDB.get().getTasks();
+        assertNotNull(taskGoalDB);
+        assertEquals(1, taskGoalDB.size());
+        Task firstTask = taskGoalDB.iterator().next();
+        assertNotNull(firstTask);
+        assertEquals(task.getName(), firstTask.getName());
+        Goal firstTaskGoal = firstTask.getGoal();
+        assertNotNull(firstTaskGoal);
+        assertEquals(goalDB.get().getName(),firstTaskGoal.getName());
+        assertEquals(GoalStand.PROGRESS, goalDB.get().getGoalStand());
     }
+
+
     @Test
-    void testDeleteUser(){
-        User user =new User("nourallah","nourallah@gmail.com");
-        userRepository.save(user);
-        Optional<User>found =userRepository.findById(user.getId());
-        Assertions.assertTrue(found.isPresent());
-        userRepository.deleteById(user.getId());
-        Assertions.assertTrue(userRepository.findById(user.getId()).isEmpty());
+    void saveMultipleTasks(){
+        Task task2 = new Task("attend the lectures", goal);
+        taskRepository.save(task2);
+        entityManager.flush();
+
+        entityManager.clear();
+        Optional<Goal> goalDB = goalRepository.findById(goal.getId());
+        assertTrue(goalDB.isPresent());
+        Set<Task> taskGoalDB = goalDB.get().getTasks();
+        assertNotNull(taskGoalDB);
+        assertEquals(2, taskGoalDB.size());
+        Iterator<Task> it = taskGoalDB.iterator();
+        Task retrievedTask = it.next();
+        assertNotNull(retrievedTask);
+        Task retrievedTask2 = it.next();
+        assertNotNull(retrievedTask2);
+        assertTrue(task.getName().equals(retrievedTask.getName()) ^task.getName().equals(retrievedTask2.getName()));
+        assertTrue(task2.getName().equals(retrievedTask.getName()) ^task2.getName().equals(retrievedTask2.getName()));
+        assertEquals(goalDB.get().getName(), retrievedTask.getGoal().getName());
+        assertEquals(goalDB.get().getName(), retrievedTask2.getGoal().getName());
+    }
+
+    @Test
+    void updateTaskName(){
+        task.setName("attend the lecture");
+        taskRepository.save(task);
+        entityManager.flush();
+
+        entityManager.clear();
+        Optional<Task> savedTask = taskRepository.findById(task.getId());
+        assertTrue(savedTask.isPresent());
+        assertEquals(savedTask.get().getName(), "attend the lecture");
+
+        //check that there is only one saved task related to that goal
+        Optional<Goal> savedGoal = goalRepository.findById(goal.getId());
+        assertTrue(savedGoal.isPresent());
+        assertEquals(1, savedGoal.get().getTasks().size());
+    }
+
+    //case1: deletion for a task corresponding to a one-task goal
+    @Test
+    void deleteTask1(){
+        taskRepository.deleteById(task.getId());
+        entityManager.flush();
+
+        entityManager.clear();
+        Optional<Task> deletedTask = taskRepository.findById(task.getId());
+        assertTrue(deletedTask.isEmpty());
+
+        Optional<Goal> goalAfterDeletion = goalRepository.findById(goal.getId());
+        assertTrue(goalAfterDeletion.isPresent());
+        assertEquals(0, goalAfterDeletion.get().getTasks().size());
+        assertEquals(GoalStand.NOT_STARTED, goalAfterDeletion.get().getGoalStand());
     }
 }
