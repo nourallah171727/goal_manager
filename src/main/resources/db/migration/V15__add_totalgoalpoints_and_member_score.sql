ac0311b11a415beeccaf6d22bced86b41bc3c573
Your Name <gargourinourallah@gmail.com>
2025-09-19 22:48:06 +0100

    added points logic to database

---
+++ src/main/resources/db/migration/V15__add_totalgoalpoints_and_member_score.sql
@@ -0,0 +1,7 @@
+-- Add column to store the total points of a goal
+ALTER TABLE goals
+    ADD COLUMN totalgoalpoints BIGINT NOT NULL DEFAULT 0 ;
+
+-- Add column to store the score of each user per goal
+ALTER TABLE goal_members
+    ADD COLUMN score BIGINT NOT NULL DEFAULT 0 ;
